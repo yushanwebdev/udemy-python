0e0af656c64feb141ceaaae2923a2afd5a8dde95
yushan <yushan@intervest.lk>
2021-06-04 21:41:53 +0530

    feat: Learn about basic data types

---
+++ dataTypes.py
@@ -0,0 +1,21 @@
+# Integers
+print(12978)
+print(-12978)
+
+# Floats
+print(156.41545)
+print(-156.41545)
+
+# Booleans
+print(True)
+print(False)
+
+# Conditions 
+print(100 < 200)
+
+# Imaginery Numbers
+print(2 + 3j + 6 - 5j)
+
+# Strings
+print("Hello\nWorld")
+print("Special character \\n will add a new line.")
